b5fa514610030598a55036c8f0f015bf29151b2e
정우혁 <cj0336j@gmail.com>
2024-12-24 14:20:34 +0900

    site.config 수정정

--- site.config.js
+++ site.config.js
@@ -1,13 +1,12 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "Jacob",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    role: "Data Scientist",
+    bio: "I develop everything using Python.",
+    email: "cj0336j@gmail.com",
+    github: "Jacob",
     instagram: "",
   },
   projects: [
@@ -18,8 +17,8 @@ const CONFIG = {
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "Jacob-log",
+    description: "welcome to Jacob-log!",
     scheme: "dark", // 'light' | 'dark' | 'system'
   },
 
