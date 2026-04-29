2b34b9ab1fc177750bed8d6a651930777e433858
Jacob <cj0336j@gmail.com>
2026-04-29 22:05:05 +0900

    Debug: test queryCollection explicitly

--- src/pages/api/debug-notion.ts
+++ src/pages/api/debug-notion.ts
@@ -13,16 +13,37 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   const response = await api.getPage(id)
   const uuid = idToUuid(id)
 
-  const collectionQuery = response.collection_query
-  const blockEntry = response.block[uuid]?.value as any
-  const blockData = blockEntry?.value ?? blockEntry
+  const collectionId = Object.keys(response.collection ?? {})[0]
+  const collectionViewId = Object.keys(response.collection_view ?? {})[0]
+
+  let queryResult: any = null
+  let queryError: any = null
+  try {
+    queryResult = await (api as any).queryCollection({
+      collectionId,
+      collectionViewId,
+      query: {},
+      loader: {
+        type: "reducer",
+        reducers: {
+          collection_group_results: {
+            type: "results",
+            limit: 999,
+          },
+        },
+        searchQuery: "",
+        userTimeZone: "Asia/Seoul",
+      },
+    })
+  } catch (e: any) {
+    queryError = e?.message
+  }
 
   res.json({
-    responseTopLevelKeys: Object.keys(response),
-    blockType: blockData?.type,
-    collectionKeys: Object.keys(response.collection ?? {}),
-    collectionQueryKeys: Object.keys(collectionQuery ?? {}),
-    hasCollectionQuery: Object.keys(collectionQuery ?? {}).length > 0,
-    pageId: uuid,
+    collectionId,
+    collectionViewId,
+    queryResultKeys: queryResult ? Object.keys(queryResult) : null,
+    queryResultResult: queryResult?.result,
+    queryError,
   })
 }
