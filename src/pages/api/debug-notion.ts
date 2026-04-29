e96f9e9469a03cdbeb973b3b360ace439968e8e9
Jacob <cj0336j@gmail.com>
2026-04-29 22:02:51 +0900

    Update debug endpoint to show full response structure

--- src/pages/api/debug-notion.ts
+++ src/pages/api/debug-notion.ts
@@ -14,13 +14,15 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   const uuid = idToUuid(id)
 
   const collectionQuery = response.collection_query
-  const firstCollection = Object.values(collectionQuery ?? {})[0]
-  const firstView = firstCollection ? Object.values(firstCollection)[0] : null
+  const blockEntry = response.block[uuid]?.value as any
+  const blockData = blockEntry?.value ?? blockEntry
 
   res.json({
+    responseTopLevelKeys: Object.keys(response),
+    blockType: blockData?.type,
+    collectionKeys: Object.keys(response.collection ?? {}),
     collectionQueryKeys: Object.keys(collectionQuery ?? {}),
-    firstCollectionViewKeys: firstCollection ? Object.keys(firstCollection) : [],
-    firstViewKeys: firstView ? Object.keys(firstView as object) : [],
-    firstViewSample: firstView,
+    hasCollectionQuery: Object.keys(collectionQuery ?? {}).length > 0,
+    pageId: uuid,
   })
 }
