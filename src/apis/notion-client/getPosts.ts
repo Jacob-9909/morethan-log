674768109d1e8d5698d4bf9a9f6fbe9caf71bb71
Jacob <cj0336j@gmail.com>
2026-04-29 22:11:15 +0900

    Fix getPosts to use getCollectionData explicitly instead of relying on collection_query

--- src/apis/notion-client/getPosts.ts
+++ src/apis/notion-client/getPosts.ts
@@ -2,15 +2,9 @@ import { CONFIG } from "site.config"
 import { NotionAPI } from "notion-client"
 import { idToUuid } from "notion-utils"
 
-import getAllPageIds from "src/libs/utils/notion/getAllPageIds"
 import getPageProperties from "src/libs/utils/notion/getPageProperties"
 import { TPosts } from "src/types"
 
-/**
- * @param {{ includePages: boolean }} - false: posts only / true: include pages
- */
-
-// TODO: react query를 사용해서 처음 불러온 뒤로는 해당데이터만 사용하도록 수정
 export const getPosts = async () => {
   let id = CONFIG.notionConfig.pageId as string
   const api = new NotionAPI({
@@ -22,44 +16,53 @@ export const getPosts = async () => {
   id = idToUuid(id)
   const collectionValue = Object.values(response.collection)[0]?.value as any
   const collection = collectionValue?.value ?? collectionValue
-  const block = response.block
   const schema = collection?.schema
 
-  const blockValue = (block[id].value as any)?.value ?? block[id].value
-  const rawMetadata = blockValue
+  const blockValue = (response.block[id].value as any)?.value ?? response.block[id].value
 
-  // Check Type
   if (
-    rawMetadata?.type !== "collection_view_page" &&
-    rawMetadata?.type !== "collection_view"
+    blockValue?.type !== "collection_view_page" &&
+    blockValue?.type !== "collection_view"
   ) {
     return []
-  } else {
-    // Construct Data
-    const pageIds = getAllPageIds(response)
-    const data = []
-    for (let i = 0; i < pageIds.length; i++) {
-      const id = pageIds[i]
-      const properties = (await getPageProperties(id, block, schema)) || null
-      // Add fullwidth, createdtime to properties
-      const pageBlockValue = (block[id].value as any)?.value ?? block[id].value
-      properties.createdTime = new Date(
-        pageBlockValue?.created_time
-      ).toString()
-      properties.fullWidth =
-        (pageBlockValue?.format as any)?.page_full_width ?? false
-
-      data.push(properties)
-    }
-
-    // Sort by date
-    data.sort((a: any, b: any) => {
-      const dateA: any = new Date(a?.date?.start_date || a.createdTime)
-      const dateB: any = new Date(b?.date?.start_date || b.createdTime)
-      return dateB - dateA
-    })
-
-    const posts = data as TPosts
-    return posts
   }
+
+  const collectionId = Object.keys(response.collection)[0]
+  const collectionViewId = Object.keys(response.collection_view)[0]
+
+  // getPage does not populate collection_query, so fetch explicitly
+  const collectionData = await (api as any).getCollectionData(
+    collectionId,
+    collectionViewId,
+    collection,
+    { limit: 999 }
+  )
+
+  const pageIds: string[] =
+    collectionData?.result?.reducerResults?.collection_group_results?.blockIds ?? []
+
+  const block = { ...response.block, ...(collectionData?.recordMap?.block ?? {}) }
+
+  const data = []
+  for (let i = 0; i < pageIds.length; i++) {
+    const pageId = pageIds[i]
+    const properties = (await getPageProperties(pageId, block, schema)) || null
+    if (!properties) continue
+
+    const pageBlockValue = (block[pageId]?.value as any)?.value ?? block[pageId]?.value
+    if (!pageBlockValue) continue
+
+    properties.createdTime = new Date(pageBlockValue?.created_time).toString()
+    properties.fullWidth = (pageBlockValue?.format as any)?.page_full_width ?? false
+
+    data.push(properties)
+  }
+
+  data.sort((a: any, b: any) => {
+    const dateA: any = new Date(a?.date?.start_date || a.createdTime)
+    const dateB: any = new Date(b?.date?.start_date || b.createdTime)
+    return dateB - dateA
+  })
+
+  return data as TPosts
 }
